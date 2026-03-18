87a072a547fd5248595f88ec5755b36698132c61
Thomas Luizon Rodrigues Gregorio <thomaslrgregorio@gmail.com>
2026-03-18 13:43:50 -0300

    chore(auth): add migration to remove PasswordHash column

---
+++ src/Orbit.Infrastructure/Migrations/20260318164346_RemovePasswordHash.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Orbit.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovePasswordHash : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "Users");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordHash",
+                table: "Users",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
