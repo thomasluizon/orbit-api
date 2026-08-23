3a12b726070a25051f1ec85fe9650d9d28e915f3
Thomas Luizon Rodrigues Gregorio <thomaslrgregorio@gmail.com>
2026-08-23 18:11:12 -0300

    Raise minimum supported app version floor

---
+++ src/Orbit.Infrastructure/Migrations/20260823210933_RaiseMinSupportedVersionFloor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Orbit.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RaiseMinSupportedVersionFloor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AppConfigs",
+                keyColumn: "Key",
+                keyValue: "MinSupportedVersion",
+                column: "Value",
+                value: "1.3.11");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AppConfigs",
+                keyColumn: "Key",
+                keyValue: "MinSupportedVersion",
+                column: "Value",
+                value: "0.0.0");
+        }
+    }
+}
