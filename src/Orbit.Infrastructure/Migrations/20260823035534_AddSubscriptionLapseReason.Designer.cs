eec0782d38e4c691c195920045952b02c27dd5db
Thomas Luizon Rodrigues Gregorio <72260917+thomasluizon@users.noreply.github.com>
2026-08-23 01:52:57 -0300

    Persist subscription lapse reasons for #344 (#486)
    * Open work for ticket 344
    
    * Persist subscription lapse reasons
    
    * Refresh architecture map
    
    * Trigger checks for generated map
    
    * Cover subscription lapse mappings
    
    * Refresh coverage architecture map
    
    * Protect subscription lapse state ordering

--- src/Orbit.Infrastructure/Migrations/20260823035534_AddSubscriptionLapseReason.Designer.cs
+++ src/Orbit.Infrastructure/Migrations/20260823035534_AddSubscriptionLapseReason.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Orbit.Infrastructure.Persistence;
@@ -11,9 +12,11 @@ using Orbit.Infrastructure.Persistence;
 namespace Orbit.Infrastructure.Migrations
 {
     [DbContext(typeof(OrbitDbContext))]
-    partial class OrbitDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260823035534_AddSubscriptionLapseReason")]
+    partial class AddSubscriptionLapseReason
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -2029,9 +2032,6 @@ namespace Orbit.Infrastructure.Migrations
 
                     b.Property<string>("StripeCustomerId")
                         .HasColumnType("text");
-
-                    b.Property<DateTime?>("StripeSubscriptionEventCreatedAtUtc")
-                        .HasColumnType("timestamp with time zone");
 
                     b.Property<string>("StripeSubscriptionId")
                         .HasColumnType("text");
